cb93a5ae1cb7215e9df7483246c51b5d7f389364
Asia <asia.a.le.work@gmail.com>
2022-02-16 16:17:25 -0800

    small fix

--- react-app/src/store/questionnaire.js
+++ react-app/src/store/questionnaire.js
@@ -34,7 +34,6 @@ const getQue = que => ({
 
 export const get_questionnaire = (user_id) => async dispatch => {
     const res = await fetch(`/api/questionnaire/${user_id}`);
-    console.log(res);
     if (res.ok) {
         const data = await res.json();
         dispatch(getQue(data));
