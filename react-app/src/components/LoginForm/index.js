f4315d31c2acfc45f7195e1c34a5f31650d1e7d4
Asia <asia.a.le.work@gmail.com>
2022-02-24 10:04:28 -0800

    login/signup styling

---
+++ react-app/src/components/LoginForm/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink, Redirect } from 'react-router-dom';
+import { login } from '../../store/session';
+import './loginform.css';
+
+const LoginForm = () => {
+  const [errors, setErrors] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const user = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login(email, password));
+    if (data) {
+      setErrors(data);
+    }
+  };
+
+  const handleDemoLogin = async (e) => {
+    e.preventDefault();
+    await dispatch(login('demo@aa.io', 'password'));
+  };
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  if (user) {
+    return <Redirect to='/' />;
+  }
+
+  return (
+    <div className='login-container'>
+      <NavLink to='/'>
+        <img src='https://i.imgur.com/G90ynzP.png' id='site-logo' />
+      </NavLink>
+      <div>
+        {errors.map((error, ind) => (
+          <div key={ind}>{error}</div>
+        ))}
+      </div>
+      <form onSubmit={onLogin} id='login-form'>
+        <div id='email-div'>
+          <label htmlFor='email'>Email</label>
+          <input
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div id='pw-div'>
+          <label htmlFor='password'>Password</label>
+          <input
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+        <div id='form-buttons'>
+          <button type='submit'>Login</button>
+          <button onClick={handleDemoLogin} id='login-demo'>Demo User</button>
+        </div>
+      </form>
+      <p id='new-text'>New to the site?</p>
+      <NavLink to='/register'>Create an account!</NavLink>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ react-app/src/components/SignUpForm/index.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { Redirect, NavLink } from 'react-router-dom';
+import { signUp, login } from '../../store/session';
+import './signupform.css';
+
+const SignUpForm = () => {
+  const [errors, setErrors] = useState([]);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const user = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  const onSignUp = async (e) => {
+    e.preventDefault();
+    if (password === repeatPassword) {
+      const data = await dispatch(signUp(username, email, password));
+      if (data) {
+        setErrors(data)
+      }
+    }
+  };
+
+  const handleDemoLogin = async (e) => {
+    e.preventDefault();
+    await dispatch(login('demo@aa.io', 'password'));
+  };
+
+  const updateUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const updateRepeatPassword = (e) => {
+    setRepeatPassword(e.target.value);
+  };
+
+  if (user) {
+    return <Redirect to='/' />;
+  }
+
+  return (
+    <div className='register-container'>
+      <NavLink to='/'>
+        <img src='https://i.imgur.com/G90ynzP.png' id='site-logo' />
+      </NavLink>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+      <form onSubmit={onSignUp} id='register-form'>
+        <div className='signup-div'>
+          <label>User Name</label>
+          <input
+            type='text'
+            name='username'
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        </div>
+        <div className='signup-div'>
+          <label>Email</label>
+          <input
+            type='text'
+            name='email'
+            onChange={updateEmail}
+            value={email}
+          ></input>
+        </div>
+        <div className='signup-div'>
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            onChange={updatePassword}
+            value={password}
+          ></input>
+        </div>
+        <div className='signup-div'>
+          <label>Repeat Password</label>
+          <input
+            type='password'
+            name='repeat_password'
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+          ></input>
+        </div>
+        <div id='signup-buttons'>
+          <button type='submit'>Sign Up</button>
+          <button id='login-demo' onClick={handleDemoLogin}>Demo User</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
